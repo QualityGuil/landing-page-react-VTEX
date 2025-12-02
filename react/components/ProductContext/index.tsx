5c570ad2fb49f2f9b9ac1f6bd1f554b3cfa577ed
GuilFran Aspire7 <guilfranca27@gmail.com>
2025-12-02 10:04:07 -0300

    Utilizando do dispatch para alterar a quantity da página de produto

--- react/components/ProductContext/index.tsx
+++ react/components/ProductContext/index.tsx
@@ -1,12 +1,30 @@
-import React from "react";
-import { useProduct } from "vtex.product-context";
+import React, { useEffect } from "react";
+import { useProduct, useProductDispatch } from "vtex.product-context";
 
 const ProductContext = () => {
     const productContext = useProduct();
 
+    const dispatch = useProductDispatch();
+
+    useEffect(() => {
+        // Está retornando um erro pela possibilidade do dispatch ser null
+        if (!dispatch) {
+            console.log('dispatch está null');
+        } else {
+            dispatch({
+                type: "SET_QUANTITY",
+                args: { quantity: 50 }
+            })
+        }
+    }, []);
+
     console.log('productContext🚀: ', productContext);
 
-    return <>{productContext?.product?.productName}</>
+    return <h1>{productContext?.product?.productName}</h1>
+
+    // Não funciona importando diretamente o objeto product
+    // const { product } = useProduct();
+    // return <>{product?.productName}</>
 }
 
 export default ProductContext;
